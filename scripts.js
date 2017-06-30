8b113c4cd1e57fd9c77235f3e6d45615cbfa1b8d
Ian <iangawianski12@gmail.com>
2017-06-29 21:40:34 -0300

    new commit

--- scripts.js
+++ scripts.js
@@ -1,52 +1,116 @@
 
+var tareas = [];
+var id = 0;
 var inputTitulo = document.querySelector(".a-titulo");
 var inputDescripcion = document.querySelector(".a-descripcion");
 var botonAgregar = document.querySelector(".agregar");
+var botonBuscar = document.querySelector(".buscar");
+var botonMostrarTodas = document.querySelector(".mostrar");
 
-
-function Tarea (titulo, descripcion) {
+function Tarea (titulo, descripcion, id) {
     this.titulo = titulo;
     this.descripcion = descripcion;
+    this.id = id;
     this.contenedor;
 }
 
 function Lista (){
-    this.ul = document.querySelector("ul");
+    this.divLista = document.querySelector(".lista1");
+    this.inicializar = function (tarea, lista) {
+    	var lista = this;
+
+		botonAgregar.addEventListener("click", function() {
+			lista.agregarTarea();
+		})
+
+		botonBuscar.addEventListener("click", function () {
+			lista.buscarTarea(lista);
+		});	
+
+		botonMostrarTodas.addEventListener("click", function () {
+			lista.divLista.innerHTML = "";
+
+			for (tarea in tareas) {
+				lista.imprimirTarea(tareas[tarea], lista);
+			}
+
+		});
+    }
     this.agregarTarea = function () {
         var lista = this;
 
+        id++
         var titulo = inputTitulo.value;
         var descripcion = inputDescripcion.value;
 
-        var tarea = new Tarea (titulo, descripcion);
+        var tarea = new Tarea (titulo, descripcion, id);
 
-        var li = document.createElement("LI");
-        var h2 = document.createElement("H2");
-        var p = document.createElement("P");
-        var botonBorrar = document.createElement("BUTTON");
+        tareas.push(tarea);
 
-        botonBorrar.innerText = "Borrar tarea";
-        h2.innerText = tarea.titulo;
-        p.innerText = tarea.descripcion;
+        lista.imprimirTarea(tarea, this);
+                
+    };
+    this.imprimirTarea = function (tarea, lista) {
+    	var h6 = document.createElement("H6");
+		var li = document.createElement("DIV");
+		var h2 = document.createElement("H2");
+		var p = document.createElement("P");
+		var botonBorrar = document.createElement("BUTTON");
+		var botonEditar = document.createElement("BUTTON");
+		var botonCompletado = document.createElement("BUTTON");
 
-        li.appendChild(h2);
-        li.appendChild(p);
-        li.appendChild(botonBorrar);
+		h6.innerText = tarea.id;
+		li.classList.add("tarea");
+		li.classList.add("text-center");
+		botonEditar.innerHTML = "<span class='ion-edit'></span>";
+		botonBorrar.innerHTML = "<span class='ion-close-round'></span>";
+		botonCompletado.innerText = "Done";
+		h2.innerText = tarea.titulo;
+		p.innerText = tarea.descripcion;
 
-        tarea.contenedor = li;
+		li.appendChild(h6);
+		li.appendChild(h2);
+		li.appendChild(p);
+		li.appendChild(botonBorrar);
+		li.appendChild(botonEditar);
+		li.appendChild(botonCompletado);
 
-        this.ul.appendChild(li);
+		tarea.contenedor = li;
 
+		miLista.divLista.appendChild(li);
 
-        botonBorrar.addEventListener("click", function() {
-            lista.ul.removeChild(tarea.contenedor);
-        })
+		botonCompletado.addEventListener("click", function() {
+			li.classList.toggle("completado");
+		})
+
+		botonBorrar.addEventListener("click", function() {
+		    lista.divLista.removeChild(tarea.contenedor);
+		})
+
+		botonEditar.addEventListener("click", function() {
+			h2.innerText = prompt("Editar titulo");
+		    p.innerText = prompt("Editar descripcion");
+		})
+    };
+    this.buscarTarea = function (lista) {
+    	var idABuscar = parseInt(prompt("Decime el ID."));
+
+		lista.divLista.innerHTML = "";
+
+		for (tarea in tareas) {
+			if (tareas[tarea].id === idABuscar) {
+				var tareaAMostrar = tareas[tarea];
+				console.log(tareaAMostrar);
+			} 
+	 	}
+
+		if (tareaAMostrar) {
+			lista.imprimirTarea(tareaAMostrar, lista);
+		} else {
+			alert("No existe!");
+		}
     }
 }
 
 var miLista = new Lista();
-
-
-botonAgregar.addEventListener("click", function() {
-			miLista.agregarTarea();
-		})
+miLista.inicializar();
